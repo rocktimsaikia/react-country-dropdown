5844373f5d6a2d1eeddd2d8fbfd5ea0c134d98ff
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-01 12:44:11 +0530

    feat(storybook): add storybook setup and update .gitignore

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+	parameters: {
+		layout: "centered",
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;

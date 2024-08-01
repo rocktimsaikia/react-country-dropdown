28a7f63c3b76dbb75216e994325c5137876e1c07
Rocktim Saikia <rocktimthedev@gmail.com>
2024-08-02 03:34:04 +0530

    build: switch build system to vite and update dependencies

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: "src/index.jsx",
+			name: "ReactCountryDropdown",
+			fileName: (format) => `react-country-dropdown.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
+});
